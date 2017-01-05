6dcb6a2d47e3a892c80bf402db7a3f42c5ca3fd5
ChristianBreitkreutz <cbreitkreutz@epages.com>
2017-01-05 22:29:44 +0100

    add all the shit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+wurst - It's new $module
+
+# SYNOPSIS
+
+    use wurst;
+
+# DESCRIPTION
+
+wurst is ...
+
+# LICENSE
+
+Copyright (C) ChristianBreitkreutz.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+ChristianBreitkreutz <cbreitkreutz@epages.com>
---
+++ lib/wurst.pm
@@ -0,0 +1,39 @@
+package wurst;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+wurst - It's new $module
+
+=head1 SYNOPSIS
+
+    use wurst;
+
+=head1 DESCRIPTION
+
+wurst is ...
+
+=head1 LICENSE
+
+Copyright (C) ChristianBreitkreutz.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+ChristianBreitkreutz E<lt>cbreitkreutz@epages.comE<gt>
+
+=cut
+
